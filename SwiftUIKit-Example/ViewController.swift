ee25a41f66268983af93c10933c2a2a00bc10442
zmeriksen <zmeriksen@gmail.com>
2019-10-29 13:23:10 -0500

    Initial Commit

---
+++ SwiftUIKit-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUIKit-Example
+//
+//  Created by Zach Eriksen on 10/29/19.
+//  Copyright © 2019 oneleif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
