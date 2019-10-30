65d039881ba28c9448616f0a4ab7838c6088eb40
Zach Eriksen <zeriksen@clientresourcesinc.com>
2019-10-30 13:22:08 -0500

    Verison Bump and Added more options to Button

--- Sources/SwiftUIKit/Views/Button.swift
+++ Sources/SwiftUIKit/Views/Button.swift
@@ -11,11 +11,15 @@ public class Button: UIButton {
     private var action: () -> Void
     
     public init(_ title: String,
-                action: @escaping () -> Void,
-                forEvent event: UIControl.Event = .touchUpInside) {
+                titleColor: UIColor = .white,
+                backgroundColor: UIColor? = nil,
+                forEvent event: UIControl.Event = .touchUpInside,
+                action: @escaping () -> Void) {
         self.action = action
         super.init(frame: .zero)
         
+        self.backgroundColor = backgroundColor
+        self.setTitleColor(titleColor, for: .normal)
         self.setTitle(title, for: .normal)
         self.addTarget(self, action: #selector(handleButtonTap), for: event)
     }
