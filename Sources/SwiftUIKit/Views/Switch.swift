910099080ebf2fe13abf10c786b2da153696ca77
zmeriksen <zmeriksen@gmail.com>
2019-11-02 17:40:59 -0500

    Added Switch

---
+++ Sources/SwiftUIKit/Views/Switch.swift
@@ -0,0 +1,30 @@
+//
+//  Switch.swift
+//  
+//
+//  Created by Zach Eriksen on 11/2/19.
+//
+
+import UIKit
+
+@available(iOS 9.0, *)
+public class Switch: UISwitch {
+    private var toggleChangedHandler: ((Bool) -> Void)?
+    
+    public init(isOn: Bool = false,
+                _ toggleChanged: ((Bool) -> Void)? = nil) {
+        super.init(frame: .zero)
+        
+        self.isOn = isOn
+        self.toggleChangedHandler = toggleChanged
+        addTarget(self, action: #selector(handleValueChanged), for: .valueChanged)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func handleValueChanged() {
+        toggleChangedHandler?(isOn)
+    }
+}
