1f1da7cff4ed075d7d38fadb34ccbca4a9ef7ad3
zmeriksen <zmeriksen@gmail.com>
2019-11-04 17:44:52 -0600

    Added Slider

---
+++ Sources/SwiftUIKit/Views/Slider.swift
@@ -0,0 +1,36 @@
+//
+//  Slider.swift
+//  
+//
+//  Created by Zach Eriksen on 11/4/19.
+//
+
+import UIKit
+
+@available(iOS 9.0, *)
+public class Slider: UISlider {
+    private var valueChangedHandler: ((Float) -> Void)?
+    
+    public init(value: Float,
+                from: Float,
+                to: Float,
+                valueChangedHandler: ((Float) -> Void)? = nil) {
+        
+        super.init(frame: .zero)
+        
+        self.value = value
+        self.minimumValue = from
+        self.maximumValue = to
+        
+        self.valueChangedHandler = valueChangedHandler
+        addTarget(self, action: #selector(handleValueChanged), for: .valueChanged)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func handleValueChanged(sender: UISlider) {
+        valueChangedHandler?(sender.value)
+    }
+}
