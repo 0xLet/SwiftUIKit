c8b877db7f17f1c6804bb7b2f643fff6ba5c94af
zmeriksen <zmeriksen@gmail.com>
2019-11-04 19:21:24 -0600

    Added Field

---
+++ Sources/SwiftUIKit/Views/Field.swift
@@ -0,0 +1,63 @@
+//
+//  Field.swift
+//  
+//
+//  Created by Zach Eriksen on 11/4/19.
+//
+
+import UIKit
+
+@available(iOS 9.0, *)
+public class Field: UITextField {
+    public typealias WillValueChangeHandler = (_ sender: UITextField, _ newValue: String, _ input: String) -> Bool
+    public typealias DidValueChangeHandler = (String) -> Void
+    
+    private var willInputUpdateHandler: WillValueChangeHandler?
+    private var inputHandler: DidValueChangeHandler?
+    
+    public init(value: String,
+                placeholder: String,
+                keyboardType type: UIKeyboardType) {
+        
+        super.init(frame: .zero)
+        
+        self.text = value
+        self.placeholder = placeholder
+        self.keyboardType = type
+        
+        delegate = self
+        addTarget(self, action: #selector(valueDidChange), for: .editingChanged)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @discardableResult
+    public func willInputUpdateHandler(_ willInputUpdateHandler: @escaping WillValueChangeHandler) -> Self {
+        self.willInputUpdateHandler = willInputUpdateHandler
+        
+        return self
+    }
+    
+    @discardableResult
+    public func inputHandler(_ inputHandler: @escaping DidValueChangeHandler) -> Self {
+        self.inputHandler = inputHandler
+        
+        return self
+    }
+    
+    @objc private func valueDidChange(_ textField: UITextField) {
+        inputHandler?(textField.text ?? "")
+    }
+}
+
+@available(iOS 9.0, *)
+extension Field: UITextFieldDelegate {
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let newValue = NSString(string: textField.text ?? "").replacingCharacters(in: range, with: string)
+        
+        return willInputUpdateHandler?(textField, newValue, string) ?? true
+    }
+}
+
