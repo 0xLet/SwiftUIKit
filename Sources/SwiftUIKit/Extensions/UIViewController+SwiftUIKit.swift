5e5ecb62865187bc40ab2112707405c5ca47878e
zmeriksen <zmeriksen@gmail.com>
2019-12-01 21:27:49 -0600

    Adding ViewController

--- Sources/SwiftUIKit/Extensions/UIViewController+SwiftUIKit.swift
+++ Sources/SwiftUIKit/Extensions/UIViewController+SwiftUIKit.swift
@@ -8,6 +8,43 @@
 import UIKit
 
 @available(iOS 9.0, *)
+public class ViewController: UIViewController {
+    private var closure: () -> UIView
+    
+    public init(_ closure: @escaping (() -> UIView)) {
+        self.closure = closure
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.embed(closure)
+    }
+    
+    public override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    public override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    public override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+}
+
+@available(iOS 9.0, *)
 public extension UIViewController {
     convenience init(_ closure: (() -> UIView)) {
         self.init()
