235fa41cef7153e47e1bd3d79c210303a37ccfa1
zmeriksen <zmeriksen@gmail.com>
2019-12-02 21:33:45 -0600

    Added customView button

--- Sources/SwiftUIKit/Views/Button.swift
+++ Sources/SwiftUIKit/Views/Button.swift
@@ -27,6 +27,19 @@ public class Button: UIButton {
         accessibility(label: title, traits: .button)
     }
     
+    public init(_ action: @escaping () -> Void,
+                forEvent event: UIControl.Event = .touchUpInside,
+                _ closure: () -> UIView) {
+        self.action = action
+        super.init(frame: .zero)
+        
+        embed {
+            closure()
+        }
+        
+        self.addTarget(self, action: #selector(handleButtonTap), for: event)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
