7aa6b72327379333fcbeaadcbd21c3b3124b56c8
zmeriksen <zmeriksen@gmail.com>
2019-12-11 19:48:31 -0600

    Added Label.init for AttributedStrings

--- Sources/SwiftUIKit/Views/Label.swift
+++ Sources/SwiftUIKit/Views/Label.swift
@@ -22,6 +22,17 @@ public class Label: UILabel {
         accessibility(label: text, traits: .staticText)
     }
     
+    public init(_ attributedText: AttributedString) {
+        super.init(frame: .zero)
+        
+        self.attributedText = attributedText
+        
+        if #available(iOS 10.0, *) {
+            adjustsFontForContentSizeCategory = true
+        }
+        accessibility(label: attributedText.mutableString.description, traits: .staticText)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
