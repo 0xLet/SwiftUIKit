e7754ac5582fa5b0ef18e5d2367f5761b387b862
zmeriksen <zmeriksen@gmail.com>
2019-12-11 20:07:26 -0600

    Added apply function for Label

--- Tests/SwiftUIKitTests/SwiftUIKitTests.swift
+++ Tests/SwiftUIKitTests/SwiftUIKitTests.swift
@@ -61,6 +61,33 @@ final class SwiftUIKitTests: XCTestCase {
         XCTAssert(!(label.accessibilityLabel?.isEmpty ?? true))
     }
     
+    func testApplyLabel() {
+        var usernameAttributes = StringAttributes(for: .font, value: UIFont.preferredFont(forTextStyle: .headline))
+        usernameAttributes.add(key: .foregroundColor, value: UIColor.red)
+        let captionAttributes = StringAttributes {
+            [
+                .font: UIFont.preferredFont(forTextStyle: .caption1),
+                .foregroundColor: UIColor.darkGray
+            ]
+        }
+        let caption = AttributedString(string: "oneleif is a project based group focused on learning and mentorship. Our core tenet of becoming skilled professionals is to work on open source projects. Open source simply means the work you are doing is available to the public. This comes with the benefit that anyone can help you on your project, and allows those without experience to see how something is made.", attributes: captionAttributes)
+        
+        caption.set(attributes: usernameAttributes, range: 0 ... 4)
+        
+        let initLabel = Label(caption)
+            .number(ofLines: 3)
+        let applyLabel = Label("oneleif is a project based group focused on learning and mentorship. Our core tenet of becoming skilled professionals is to work on open source projects. Open source simply means the work you are doing is available to the public. This comes with the benefit that anyone can help you on your project, and allows those without experience to see how something is made.")
+            .apply(attributes: captionAttributes)
+            .apply(attributes: usernameAttributes, range: 0 ... 4)
+        
+        XCTAssert(initLabel.attributedText == applyLabel.attributedText)
+        XCTAssert(initLabel.text == applyLabel.text)
+        XCTAssert(initLabel.accessibilityLabel == applyLabel.accessibilityLabel)
+        
+        XCTAssert(!(applyLabel.text?.isEmpty ?? true))
+        XCTAssert(!(applyLabel.accessibilityLabel?.isEmpty ?? true))
+    }
+    
     static var allTests = [
         ("testLabelADA", testLabelADA),
         ("testButtonADA", testButtonADA)
