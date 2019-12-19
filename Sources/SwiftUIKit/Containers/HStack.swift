0ffa905e93ee1ff7c26793cef6d3c1a579c6ae62
zmeriksen <zmeriksen@gmail.com>
2019-12-18 18:11:22 -0600

    HStack first draft documentation

--- Sources/SwiftUIKit/Containers/HStack.swift
+++ Sources/SwiftUIKit/Containers/HStack.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
+/// Horizontal StackView
 @available(iOS 9.0, *)
 public class HStack: UIView {
+    /// The views that the HStack contains
     public var views: [UIView] = []
     
+    /// Create a HStack
+    /// - Parameters:
+    ///     - withSpacing: The amount of spacing between each child view
+    ///     - padding: The amount of space between this view and its parent view
+    ///     - alignment: The layout of arranged views perpendicular to the stack view’s axis (source: UIStackView.Alignment)
+    ///     - distribution: The layout that defines the size and position of the arranged views along the stack view’s axis (source: UIStackView.Distribution)
+    ///     - closure: A trailing closure that accepts an array of views
     public init(withSpacing spacing: Float = 0,
          padding: Float = 0,
          alignment: UIStackView.Alignment = .fill,
@@ -27,6 +36,13 @@ public class HStack: UIView {
                closure)
     }
     
+    /// Create a HStack that accepts an array of UIView?
+    /// - Parameters:
+    ///     - withSpacing: The amount of spacing between each child view
+    ///     - padding: The amount of space between this view and its parent view
+    ///     - alignment: The layout of arranged views perpendicular to the stack view’s axis (source: UIStackView.Alignment)
+    ///     - distribution: The layout that defines the size and position of the arranged views along the stack view’s axis (source: UIStackView.Distribution)
+    ///     - closure: A trailing closure that accepts an array of optional views
     public init(withSpacing spacing: Float = 0,
          padding: Float = 0,
          alignment: UIStackView.Alignment = .fill,
