39c57f7016dce658ad7b5f583e039e07167ed716
zmeriksen <zmeriksen@icloud.com>
2020-04-12 14:54:50 -0500

    Add TableView

--- Sources/SwiftUIKit/Views/TableView.swift
+++ Sources/SwiftUIKit/Views/TableView.swift
@@ -18,3 +18,69 @@ public protocol DataConfigurable: UITableViewCell {
 public protocol CellConfigurable {
     func configure(forData data: CellDisplayable) -> UITableViewCell
 }
+
+public class TableView: UITableView {
+    public var data: [[CellDisplayable]]
+    
+    public init(initalData: [[CellDisplayable]] = [[CellDisplayable]](),
+                style: UITableView.Style = .plain) {
+        self.data = initalData
+        super.init(frame: .zero, style: style)
+        
+        dataSource = self
+        delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+public extension TableView {
+    @discardableResult
+    func update(closure: ([[CellDisplayable]]) -> [[CellDisplayable]]) -> Self {
+        data = closure(data)
+        
+        return self
+    }
+    
+    @discardableResult
+    func append(closure: () -> [[CellDisplayable]]) -> Self {
+        data += closure()
+        
+        return self
+    }
+    
+    @discardableResult
+    func reload() -> Self {
+        reloadData()
+        
+        return self
+    }
+}
+
+extension TableView: UITableViewDelegate {
+    
+}
+
+extension TableView: UITableViewDataSource {
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return data.count
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[section].count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellData = data[indexPath.section][indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellData.cellID, for: indexPath)
+        
+        if let configure = cell as? CellConfigurable {
+            return configure.configure(forData: cellData)
+        }
+        
+        return cell
+    }
+}
