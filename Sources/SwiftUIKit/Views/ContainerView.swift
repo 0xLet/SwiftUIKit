9181735c1d95cb2dd759c378d677752d8dc717f8
zmeriksen <zmeriksen@icloud.com>
2020-05-17 10:45:34 -0500

    suik-142: Added ContainerView

---
+++ Sources/SwiftUIKit/Views/ContainerView.swift
@@ -0,0 +1,43 @@
+//
+//  ContainerView.swift
+//  SwiftUIKit
+//
+//  Created by Zach Eriksen on 5/17/20.
+//
+
+import UIKit
+
+@available(iOS 9.0, *)
+public class ContainerView: UIView {
+    private weak var parentViewController: UIViewController?
+    weak var viewController: UIViewController?
+    
+    public init(parent: UIViewController, child: () -> UIViewController) {
+        parentViewController = parent
+        viewController = child()
+        super.init(frame: .zero)
+        
+        embedViewController()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+@available(iOS 9.0, *)
+private extension ContainerView {
+    func embedViewController() {
+        guard let parent = parentViewController,
+            let child = viewController else {
+                return
+        }
+        
+        parent.addChild(child)
+        child.didMove(toParent: parent)
+        
+        embed {
+            child.view
+        }
+    }
+}
