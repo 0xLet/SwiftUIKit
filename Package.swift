1aa8f2b4e3b5ac68838fce5c6f0810151fa52be7
zmeriksen <zmeriksen@icloud.com>
2020-08-29 13:46:44 -0500

    Update Later version to develop branch

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/0xLeif/Later", from: "0.3.0")
+        .package(url: "https://github.com/0xLeif/Later", .branch("develop"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
