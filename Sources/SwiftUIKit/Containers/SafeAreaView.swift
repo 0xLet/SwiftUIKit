7e3e622a4501643537207ffa9ded07f07490f8c9
Zach Eriksen <zmeriksen@icloud.com>
2021-03-25 18:31:42 -0500

    Added ignoreArchor paramters

--- Sources/SwiftUIKit/Containers/SafeAreaView.swift
+++ Sources/SwiftUIKit/Containers/SafeAreaView.swift
@@ -13,7 +13,13 @@ public class SafeAreaView: UIView {
     
     /// Create a SafeAreaView
     /// - Parameter closure: A trailing closure that accepts a view
-    public init(_ closure: () -> UIView) {
+    public init(
+        ignoreTopAnchorWithPadding topPadding: Float? = nil,
+        ignoreLeadingAnchorWithPadding leadingPadding: Float? = nil,
+        ignoreTrailingAnchorWithPadding trailingPadding: Float? = nil,
+        ignoreBottomAnchorWithPadding bottomPadding: Float? = nil,
+        _ closure: () -> UIView
+    ) {
         let view = closure()
         super.init(frame: .zero)
         
@@ -22,12 +28,32 @@ public class SafeAreaView: UIView {
         
         let margins = layoutMarginsGuide
         let guide = safeAreaLayoutGuide
-        NSLayoutConstraint.activate([
-            view.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
-            view.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
-            view.topAnchor.constraint(equalToSystemSpacingBelow: guide.topAnchor, multiplier: 1.0),
-            guide.bottomAnchor.constraint(equalToSystemSpacingBelow: view.bottomAnchor, multiplier: 1.0)
-        ])
+        
+        activateLayoutConstraints {
+            [
+                topPadding.map {
+                    view.topAnchor.constraint(equalTo: topAnchor,
+                                              constant: CGFloat($0))
+                } ?? view.topAnchor.constraint(equalToSystemSpacingBelow: guide.topAnchor,
+                                               multiplier: 1.0),
+                
+                leadingPadding.map {
+                    view.leadingAnchor.constraint(equalTo: leadingAnchor,
+                                                  constant: CGFloat($0))
+                } ?? view.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+                
+                trailingPadding.map {
+                    view.trailingAnchor.constraint(equalTo: trailingAnchor,
+                                                   constant: CGFloat(-$0))
+                } ?? view.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+                
+                bottomPadding.map {
+                    view.bottomAnchor.constraint(equalTo: bottomAnchor,
+                                                 constant: CGFloat(-$0))
+                } ?? guide.bottomAnchor.constraint(equalToSystemSpacingBelow: view.bottomAnchor,
+                                                   multiplier: 1.0)
+            ]
+        }
     }
     
     /// not implemented
