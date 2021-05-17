ed6c44a3e3afd75c5e2a78aa7ee4a0d7d985a252
Zach Eriksen <zmeriksen@icloud.com>
2021-05-17 10:27:14 -0500

    Revert removal of Observation

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/0xLeif/DataObject", from: "1.0.0")
+        .package(url: "https://github.com/0xLet/DataObject", from: "1.0.0"),
+        .package(url: "https://github.com/0xLet/Observation", from: "0.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -21,7 +22,8 @@ let package = Package(
         .target(
             name: "SwiftUIKit",
             dependencies: [
-                "DataObject"
+                "DataObject",
+                "Observation"
         ]),
         .testTarget(
             name: "SwiftUIKitTests",
