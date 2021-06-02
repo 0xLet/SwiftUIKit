455b9b61a8ce3dd82105eb0c85b5ae93a8c6d7ae
Zach Eriksen <zmeriksen@icloud.com>
2021-06-01 21:49:56 -0500

    Updated packages

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/0xLet/DataObject.git", from: "1.0.0"),
-        .package(url: "https://github.com/0xLet/Observation.git", from: "0.0.2")
+        .package(url: "https://github.com/0xLet/Chain.git", from: "0.3.0"),
+        .package(url: "https://github.com/0xLeif/Chronicle.git", from: "0.2.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,7 +24,8 @@ let package = Package(
             name: "SwiftUIKit",
             dependencies: [
                 "DataObject",
-                "Observation"
+                "Chain",
+                "Chronicle"
         ]),
         .testTarget(
             name: "SwiftUIKitTests",
