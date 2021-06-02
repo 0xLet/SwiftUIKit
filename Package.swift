4d3253e6b67c8f139fdb64512131ecca1c15524d
Zach Eriksen <zmeriksen@icloud.com>
2021-06-01 22:08:41 -0500

    Added Task and moved platform to iOS 13

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIKit",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -15,7 +18,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/0xLet/DataObject.git", from: "1.0.0"),
         .package(url: "https://github.com/0xLet/Chain.git", from: "0.3.0"),
-        .package(url: "https://github.com/0xLeif/Chronicle.git", from: "0.2.3")
+        .package(url: "https://github.com/0xLeif/Chronicle.git", from: "0.2.3"),
+        .package(url: "https://github.com/0xLeif/Task.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,7 +29,8 @@ let package = Package(
             dependencies: [
                 "DataObject",
                 "Chain",
-                "Chronicle"
+                "Chronicle",
+                "Task"
         ]),
         .testTarget(
             name: "SwiftUIKitTests",
