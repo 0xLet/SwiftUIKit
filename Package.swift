bcfd62da77d0025920eed05c7efa1d6719811c0b
Zach Eriksen <zeriksen@fnoex.com>
2021-06-18 11:40:57 -0500

    Updated DataObject to 1.0.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/0xLeif/Observation", from: "0.0.2"),
-        .package(url: "https://github.com/0xLeif/DataObject", from: "0.2.1")
+        .package(url: "https://github.com/0xLet/DataObject.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
